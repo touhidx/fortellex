bdd9275ae0817728729f7f52d5087cff1ba9809b
TouhidX <touhidul0147@gmail.com>
2025-10-29 12:18:36 +0600

    formating

--- resources/views/frontend/components/product-carousel.blade.php
+++ resources/views/frontend/components/product-carousel.blade.php
@@ -10,7 +10,7 @@
                     <ul class="glide__slides">
                         @foreach($products as $product)
                         <li class="glide__slide">
-                            @include('components.product-card', $product)
+                            @include('frontend.components.product-cart', $product)
                         </li>
                         @endforeach
                     </ul>
